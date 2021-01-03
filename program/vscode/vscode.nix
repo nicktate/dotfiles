5dab405efb94976d36f051e2e0f64abd594b39be
Nicholas Tate <ntate22@gmail.com>
2021-01-03 16:13:41 -0500

    vscode conf

--- program/vscode/vscode.nix
+++ program/vscode/vscode.nix
@@ -10,9 +10,31 @@
     # package = with pkgs; vscodium;
 
     userSettings = {
+      "go.formatTool" = "gofmt";
+      "go.lintTool" = "golint";
+      "go.lintOnSave" = "file";
+      "go.useLanguageServer" = true;
+      "[go]" = {
+        "editor.formatOnSave" = true;
+        "editor.snippetSuggestions" = "none";
+        "editor.codeActionsOnSave" = {
+          "source.organizeImports" =  true;
+        };
+      };
+      "go.toolsEnvVars" = {
+        "GOFLAGS" = "-tags=sandbox,integration,e2e";
+      };
+      "go.languageServerFlags" = [];
+      "window.zoomLevel" = 0;
+      "editor.formatOnSave" =  true;
     };
 
     extensions = with pkgs.vscode-extensions; [
+      bbenoist.Nix
+      vscodevim.vim
+      golang.Go
+      redhat.vscode-yaml
+      ms-vscode-remote.remote-ssh
     ];
   };
 }
